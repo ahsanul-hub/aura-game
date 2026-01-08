58f64e61aa3cf4d219439a41c98f993e9fe0c043
Muhammad Alfiandi Rizki <alfian.jw@gmail.com>
2026-01-08 13:22:37 +0700

    fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: 'http://103.228.118.187:4001',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
