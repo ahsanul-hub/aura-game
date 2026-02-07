80c34b7f0646c96f97bc963a888bd07b092a4637
Muhammad Alfiandi Rizki <alfian.jw@gmail.com>
2026-02-07 18:57:11 +0700

    remove bg layout

--- src/components/Banner/Layout.tsx
+++ src/components/Banner/Layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 export default function LayoutBanner({ children }: { children: ReactNode }) {
   return (
     <div className="w-full max-w-5xl mx-auto px-4 py-10 ">
-      <div className="relative group overflow-hidden rounded-2xl bg-gray-100 shadow-2xl">
+      <div className="relative group overflow-hidden rounded-2xl shadow-2xl">
         {children}
       </div>
     </div>
